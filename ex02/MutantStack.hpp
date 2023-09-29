39b3346405352e1dc0434c1b13fbbc206eff4efd
Marc <Nope>
2023-09-29 13:04:18 +0200

    better solution

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: mjourno <mjourno@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 10:43:56 by mjourno           #+#    #+#             */
-/*   Updated: 2023/09/29 12:36:26 by mjourno          ###   ########.fr       */
+/*   Updated: 2023/09/29 12:42:32 by mjourno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,32 +16,18 @@
 # include <stack>
 # include <deque>
 
-template<typename T, class Container = std::deque<T>>
+template<typename T, class Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container> {
 	public:
 		//canon
-		MutantStack();
-		MutantStack(MutantStack<T> const &src);
-		MutantStack<T> &operator=(MutantStack<T> const &src);
-		virtual ~MutantStack();
+		MutantStack() {};
+		MutantStack(MutantStack<T> const &src) {*this = src;};
+		MutantStack<T> &operator=(MutantStack<T> const &src) {std::stack<T, Container>::operator=(src); return *this;};
+		virtual ~MutantStack() {};
 
-		struct iterator {
-			private:
-				T*	m_ptr;
-			public:
-				iterator(T *ptr) : m_ptr(ptr) {}
-				T &operator*() const {return *m_ptr;}
-				T *operator->() {return m_ptr;}
-				iterator& operator++() {m_ptr++; return *this;}
-				iterator& operator--() {m_ptr--; return *this;}
-				iterator operator++(int) {iterator tmp = *this; ++(*this); return tmp;}
-				iterator operator--(int) {iterator tmp = *this; --(*this); return tmp;}
-				bool operator== (const iterator& a) { return a.m_ptr == this->m_ptr; };
-				bool operator!= (const iterator& a) { return a.m_ptr != this->m_ptr; };
-
-		};
-		iterator begin()	{return iterator(&this->top() - (this->size() - 1));}
-		iterator end()		{return iterator(&this->top() + 1);}
+		typedef typename Container::iterator	iterator;
+		iterator	begin()	{return this->c.begin();}
+		iterator	end()	{return this->c.end();}
 };
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mjourno <mjourno@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 10:43:46 by mjourno           #+#    #+#             */
-/*   Updated: 2023/09/29 12:27:19 by mjourno          ###   ########.fr       */
+/*   Updated: 2023/09/29 12:51:41 by mjourno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include "MutantStack.hpp"
 
 int main() {
+	std::cout << "MutantStack: " << std::endl;
 	MutantStack<int> mstack;
 
 	mstack.push(5);
@@ -30,7 +31,6 @@ int main() {
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
 
 	MutantStack<int>::iterator it = mstack.begin();
@@ -39,51 +39,40 @@ int main() {
 	++it;
 	--it;
 
-	std::cout << std::endl;
-
 	while (it != ite) {
 		std::cout << *it << std::endl;
 		++it;
 	}
 
 	std::stack<int> s(mstack);
+
+
+	std::cout << "\nlist: " << std::endl;
+	std::list<int> mlist;
+
+	mlist.push_back(5);
+	mlist.push_back(17);
+
+	std::cout << mlist.back() << std::endl;
+
+	mlist.pop_back();
+
+	std::cout << mlist.size() << std::endl;
+
+	mlist.push_back(3);
+	mlist.push_back(5);
+	mlist.push_back(737);
+	mlist.push_back(0);
+
+	std::list<int>::iterator it2 = mlist.begin();
+	std::list<int>::iterator ite2 = mlist.end();
+
+	++it2;
+	--it2;
+
+	while (it2 != ite2) {
+		std::cout << *it2 << std::endl;
+		++it2;
+	}
 	return 0;
 }
-
-
-
-
-//int main() {
-//	std::list<int> mstack;
-
-//	mstack.push_back(5);
-//	mstack.push_back(17);
-
-//	std::cout << mstack.back() << std::endl;
-
-//	mstack.pop_back();
-
-//	std::cout << mstack.size() << std::endl;
-
-//	mstack.push_back(3);
-//	mstack.push_back(5);
-//	mstack.push_back(737);
-//	//[...]
-//	mstack.push_back(0);
-
-//	std::list<int>::iterator it = mstack.begin();
-//	std::list<int>::iterator ite = mstack.end();
-
-//	++it;
-//	--it;
-
-//	std::cout << std::endl;
-
-//	while (it != ite) {
-//		std::cout << *it << std::endl;
-//		++it;
-//	}
-
-//	//std::stack<int> s(mstack);
-//	return 0;
-//}
